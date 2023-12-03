7dc057d07dfbdefbe40bfe827bdaa4dff032a25d
bvujovic <bv.net@outlook.com>
2023-12-03 20:05:40 +0100

    Add project files.

---
+++ EasyOCR/FrmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace EasyOCR
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(13, 13);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(108, 32);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 191);
+            this.Controls.Add(this.btnTest);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmMain";
+            this.Text = "Easy OCR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
---
+++ EasyOCR/FrmMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyOCR
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // https://www.c-sharpcorner.com/article/ocr-using-tesseract-in-C-Sharp/
+                // https://ironsoftware.com/csharp/ocr/examples/intl-languages/
+                // https://www.youtube.com/watch?v=X0wW4KyLvJ4
+                //var ocr1 = new IronOcr.IronTesseract { Language = IronOcr.OcrLanguage.Serbian };
+                //var ocr = new IronOcr.IronTesseract { Language = IronOcr.OcrLanguage.CyrillicAlphabet };
+                var ocr = new IronOcr.IronTesseract();
+                //ocr.Configuration.WhiteListCharacters = "0123456789-";
+                //var dialog = new OpenFileDialog();
+                //if (dialog.ShowDialog() != DialogResult.OK)
+                //    return;
+
+                using (var ocrInput = new IronOcr.OcrInput())
+                {
+                    //ocrInput.AddImage(dialog.FileName);
+
+                    var bmp = Clipboard.GetImage() as Bitmap;
+                    using (var ms = new MemoryStream())
+                    {
+                        bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                        ocrInput.AddImage(ms.ToArray());
+                    }
+
+                    // ocrInput.AddPdf("document.pdf");
+                    var ocrResult = ocr.Read(ocrInput);
+                    MessageBox.Show(ocrResult.Text, $"OCR ({ocrResult.Confidence:00}%)");
+                    Clipboard.SetText(ocrResult.Text);
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, "ocr"); }
+        }
+    }
+}
