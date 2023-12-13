79a954cc00b66a35838715a2fdc610c150c8e85e
bvujovic <bv.net@outlook.com>
2023-12-13 23:13:53 +0100

    Proveravati clipboard, slike automatski pretvarati u tekst Prebaciti jezik na ćirilicu

--- EasyOCR/FrmMain.Designer.cs
+++ EasyOCR/FrmMain.Designer.cs
@@ -28,7 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnTest = new System.Windows.Forms.Button();
+            this.lblClipboardImgChanges = new System.Windows.Forms.Label();
+            this.tim = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // btnTest
@@ -41,23 +44,42 @@
             this.btnTest.UseVisualStyleBackColor = true;
             this.btnTest.Click += new System.EventHandler(this.BtnTest_Click);
             // 
+            // lblClipboardImgChanges
+            // 
+            this.lblClipboardImgChanges.AutoSize = true;
+            this.lblClipboardImgChanges.Location = new System.Drawing.Point(12, 61);
+            this.lblClipboardImgChanges.Name = "lblClipboardImgChanges";
+            this.lblClipboardImgChanges.Size = new System.Drawing.Size(14, 16);
+            this.lblClipboardImgChanges.TabIndex = 1;
+            this.lblClipboardImgChanges.Text = "0";
+            // 
+            // tim
+            // 
+            this.tim.Enabled = true;
+            this.tim.Interval = 500;
+            this.tim.Tick += new System.EventHandler(this.Tim_Tick);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(455, 191);
+            this.Controls.Add(this.lblClipboardImgChanges);
             this.Controls.Add(this.btnTest);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmMain";
             this.Text = "Easy OCR";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblClipboardImgChanges;
+        private System.Windows.Forms.Timer tim;
     }
 }
 
--- EasyOCR/FrmMain.cs
+++ EasyOCR/FrmMain.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EasyOCR
@@ -26,14 +20,14 @@ namespace EasyOCR
                 // https://ironsoftware.com/csharp/ocr/examples/intl-languages/
                 // https://www.youtube.com/watch?v=X0wW4KyLvJ4
                 //var ocr1 = new IronOcr.IronTesseract { Language = IronOcr.OcrLanguage.Serbian };
-                //var ocr = new IronOcr.IronTesseract { Language = IronOcr.OcrLanguage.CyrillicAlphabet };
-                var ocr = new IronOcr.IronTesseract();
+                var ocr = new IronOcr.IronTesseract { Language = IronOcr.OcrLanguage.CyrillicAlphabet };
+                //var ocr = new IronOcr.IronTesseract();
                 //ocr.Configuration.WhiteListCharacters = "0123456789-";
                 //var dialog = new OpenFileDialog();
                 //if (dialog.ShowDialog() != DialogResult.OK)
                 //    return;
-
-                using (var ocrInput = new IronOcr.OcrInput())
+                var start = DateTime.Now;
+                using (var ocrInput = new IronOcr.OcrInput() )
                 {
                     //ocrInput.AddImage(dialog.FileName);
 
@@ -46,11 +40,39 @@ namespace EasyOCR
 
                     // ocrInput.AddPdf("document.pdf");
                     var ocrResult = ocr.Read(ocrInput);
-                    MessageBox.Show(ocrResult.Text, $"OCR ({ocrResult.Confidence:00}%)");
-                    Clipboard.SetText(ocrResult.Text);
+                    // MessageBox.Show(ocrResult.Text, $"OCR ({ocrResult.Confidence:00}%)");
+                    if (!string.IsNullOrEmpty(ocrResult.Text))
+                        Clipboard.SetText(ocrResult.Text);
                 }
+                System.Diagnostics.Debug.WriteLine((DateTime.Now - start).TotalMilliseconds);
             }
             catch (Exception ex) { MessageBox.Show(ex.Message, "ocr"); }
+        }
+
+        Size? imgSize = null;
+        int cntClipboardImgChanges = 0;
+
+        private void Tim_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Clipboard.ContainsImage())
+                {
+                    var img = Clipboard.GetImage();
+                    if (!imgSize.HasValue || imgSize.Value != img.Size)
+                    {
+                        imgSize = img.Size;
+                        lblClipboardImgChanges.Text = (++cntClipboardImgChanges).ToString();
+                        btnTest.PerformClick();
+                    }
+                }
+                else
+                    imgSize = null;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
         }
     }
 }
